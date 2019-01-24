9ce6fb732e7dacbc8b9f2fea21ba5c907c54597b
keithallives <keithallives@gmail.com>
2019-01-24 18:00:52 +0800

    Added urls.py in pages app; added pages templates folder; configured projeft urls.py

--- enrollment/settings.py
+++ enrollment/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Added Apps
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
